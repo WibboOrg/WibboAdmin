5ea45356600926f372713d5ea7f5455dbb60a705
Kodamas <superjason95@gmail.com>
2023-07-16 00:34:07 +0200

    fix: article editor

--- src/pages/article/[id].vue
+++ src/pages/article/[id].vue
@@ -36,7 +36,11 @@
                                         height: 500,
                                         menubar: true,
                                         plugins: ['advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview', 'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen', 'insertdatetime', 'media', 'table', 'help', 'wordcount'],
-                                        toolbar: 'undo redo | blocks | ' + 'bold italic backcolor | alignleft aligncenter ' + 'alignright alignjustify | bullist numlist outdent indent | ' + 'removeformat | help',
+                                        toolbar:
+                                            'undo redo | blocks | ' +
+                                            'bold italic underline forecolor | link unlink image | alignleft aligncenter ' +
+                                            'alignright alignjustify | bullist numlist outdent indent | ' +
+                                            'preview fullpage | removeformat | help',
                                         skin: 'oxide-dark',
                                         content_css: 'dark',
                                         language: 'fr_FR',
